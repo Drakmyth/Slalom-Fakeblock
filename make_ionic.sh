828d9e98ddbe620661184a92310306088931e4da
Bryan Berry <berr1167@gmail.com>
2018-09-03 13:21:59 -0700

    ionic: Add npm install cmd gen functionality

--- make_ionic.sh
+++ make_ionic.sh
@@ -14,17 +14,27 @@ API_FILE="keys.do_not_commit.json"
 API_KEY="google_maps_api"
 MAPS_VERSION="4.9.1"
 
-usage() { echo "Usage: $0 [-i <input api.json for parsing>]" 1>&2; exit 1; }
+usage() { printf "Usage: $0 \n\t-i input_api.json\tDefault=$API_FILE\n\t-m maps_api_version\tDefault=$MAPS_VERSION\n" 1>&2; exit 1; }
 
 install_ionic() {
+  i_key=$1
+  maps_v=$MAPS_VERSION
+
+  depends='npm'
+  #printf "Testing dependency: $depends "
+  if ! $(isinstalled $depends); then
+    echo "FAIL"
+    echo "Error: Dependency not met: $depends"
+    exit 1
+  fi
 
   CMD="npm install @ionic-native/core @ionic-native/google-maps@${maps_v}"
-  CMD="ionic cordova plugin add cordova-plugin-googlemaps --variable API_KEY_FOR_ANDROID=\"${api_key}\" --variable API_KEY_FOR_IOS=\"${api_key}\""
-
+  eval $CMD
+  CMD="ionic cordova plugin add cordova-plugin-googlemaps --variable API_KEY_FOR_ANDROID=\"$i_key\" --variable API_KEY_FOR_IOS=\"$i_key\""
+  eval $CMD
 }
 
 isfile() {
-  echo $1
   if [[ -f $1 ]]; then
     return 0 
   else
@@ -33,7 +43,6 @@ isfile() {
 }
 
 isinstalled() {
-  echo "Checking installed command: $1"
   if [ -x "$(command -v $1)" ]; then
     return 0
   else
@@ -46,14 +55,17 @@ parse_key_file() {
   p_key=$2
 
   depends='jq'
-  echo "Checking dependency: $depends" 
-  if isinstalled $depends; then
+  #printf "Testing dependency: $depends "
+  if $(isinstalled $depends); then
+    #echo "OK"
     CMD="jq -r '.[].${p_key}' ${p_file}"
-    echo "Running parse cmd: $CMD"
-    eval $CMD
   else
+    echo "FAIL"
     echo "Error: Dependency not met: $depends"
+    exit 1
   fi
+
+  eval $CMD
 }
 
 ### Parse CLI Options  ###
@@ -73,17 +85,19 @@ done
 shift $((OPTIND-1))
 
 # Confirm api_file exists
-if isfile $API_FILE; then
-  echo "Parsing API Credentials: $API_FILE"
-  api_key="$(parse_key_file $API_FILE $API_KEY)"
-  # Confirm receipt of api_file
-  if [[ -z $api_key ]]; then
-    echo "Error Parsing API Credentials: $API_FILE"
-    usage
-  fi
-else
-  echo "Error: Infile not found: $API_FILE"
+if ! $(isfile $API_FILE); then
+  echo "Error: Infile not found: $API_FILE\n"
   usage
 fi
 
-echo $api_key
+echo "Parsing API Credentials: $API_FILE"
+api_key="$(parse_key_file $API_FILE $API_KEY)"
+
+# Confirm receipt of api_file
+if [[ -z $api_key ]]; then
+  echo "Error Parsing API Credentials: $API_FILE"
+  usage
+fi
+
+echo "API Key: $api_key"
+install_ionic "$api_key"
